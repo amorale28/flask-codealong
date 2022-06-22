1d1ec8c7f3d56661cb4fb260f357e2a91b63d452
Aaron <>
2022-06-22 01:04:54 +0000

    Changes for 6/21: made the site

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request
+from model import get_breakfast_rating
+from datetime import datetime
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -13,4 +14,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+  props = {
+    "title":"Ariana Grande Song Matcher",
+    "first_name": "",
+  }
+  return render_template("index.html", time = datetime.now(), props = props)
+
+@app.route("/results", methods = ["GET", "POST"])
+def results():
+  if request.method == "POST":
+    print(dict(request.form))
+    user_name = request.form["name"]
+    user_feeling = int(request.form["feelingsradio"])
+    user_weather = int(request.form["weather"])
+    user_sign = int(request.form["sign"])
+    user_total = user_feeling + user_weather + user_sign
+    if user_total <= 5:
+      song_title = "No Tears Left to Cry"
+    else:
+      song_title = "Dangerous Woman"
+      
+    return render_template("results.html", user_name = user_name.title(), song_title = song_title)
+  else: 
+    return "ERROR"
+
+
+# def survey():
+#   if request.met
